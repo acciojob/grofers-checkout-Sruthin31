a8af1b433d747f1a7661195f3ee80d5cb1c32a6c
Sruthin31 <153161309+Sruthin31@users.noreply.github.com>
2025-12-16 19:44:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ const getSum = () => {
 
 	totalCell.textContent = total;
 	labelCell.textContent = 'Total Price: ';
+	newRow.id = 'ans';
 
 	newRow.append(labelCell,totalCell);
 	table.appendChild(newRow)
