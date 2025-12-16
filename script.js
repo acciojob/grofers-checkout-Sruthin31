29622610cdceb55603a163805e8418d8dc86c109
Sruthin31 <153161309+Sruthin31@users.noreply.github.com>
2025-12-16 16:39:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,23 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+  const price = document.querySelectorAll('.price');
+
+	let total = 0;
+	price.forEach((p)=>{
+		total += parseFloat(p.textContent);
+	})
+	const table = document.querySelector('table');
+	const newRow = document.querySelector("tr");
+
+	const labelCell = document.createElement("td");
+	const totalCell = document.createElement("td");
+
+	totalCell.textContent = total;
+	labelCell.textContent = 'Total Price';
+
+	newRow.append(labelCell,totalCell);
+	table.appendChild(newRow)
 };
 
 getSumBtn.addEventListener("click", getSum);
